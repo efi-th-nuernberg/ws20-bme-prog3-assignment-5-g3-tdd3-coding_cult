e84373ddd0371f70bb2737dd98a9295aa28892fc
repl.it user <replituser@example.com>
2020-11-16 10:22:56 +0000

    Assignment 5 16/11

--- Life.java
+++ Life.java
@@ -1,4 +1,12 @@
 public class Life implements ILife {
+
+    String[] cellMap = new String[]  {  "     ",
+                                        "     ",
+                                        "     ",
+                                        "     ",
+                                        "     " };
+
+
   
   public static void main(String[] args) {
     Life l = new Life(new String[] {  "     ",
@@ -9,47 +17,221 @@ public class Life implements ILife {
     l = (Life) l.nextGeneration();
   }
 
-
+  //konstruktor
   public Life() {
     nukeAll();
   }
-
+  
+  //konstruktor overload
   public Life(String[] setup) {
     this();
-    for (int y = 0; y < setup.length; y++)
-      for (int x = 0; x < setup[y].length(); x++)
-        if (setup[y].charAt(x) != ' ')
+    for (int x = 0; x < setup.length; x++) {
+      for (int y = 0; y < setup[x].length(); y++) {
+        if (setup[x].charAt(y) != ' ') {
           setAlive(x, y);
+        }
+      }
+    }
   }
-
 
   @Override
   public void nukeAll() {
     // TODO Auto-generated method stub
-
+    for (int x = 0; x < cellMap.length; x++) {
+      for (int y = 0; y < cellMap[x].length(); y++) {
+        setDead(x,y);
+      }
+    }
   }
+
 
   @Override
   public void setAlive(int x, int y) {
     // TODO Auto-generated method stub
-
+    cellMap[x] = replaceChar(cellMap[x], '*', y);
   }
 
   @Override
   public void setDead(int x, int y) {
     // TODO Auto-generated method stub
-
+    cellMap[x] = replaceChar(cellMap[x], ' ', y);
   }
 
   @Override
   public boolean isAlive(int x, int y) {
     // TODO Auto-generated method stub
+    //check if the cell isAlive...
+    if(cellMap[x].charAt(y) != ' ') {
+      return true;
+    }
     return false;
   }
 
-  @Override
+  @Override //regeln
   public ILife nextGeneration() {
     // TODO Auto-generated method stub
-    return null;
+    //printCell(cellMap);
+    Life t = new Life(cellMap);
+
+   //check every cell
+    for (int x = 0; x < cellMap.length; x++) {
+      for (int y = 0; y < cellMap[x].length(); y++) {
+         if(checkCellCountAroundCell(x, y) == 3) {
+           t.setAlive(x,y);
+         }
+         if(checkCellCountAroundCell(x, y) < 2) {
+           t.setDead(x,y);
+         }
+         if(isAlive(x,y) && ((checkCellCountAroundCell(x, y) == 2) ||
+                             (checkCellCountAroundCell(x, y) == 3))) {
+           //nothing
+         }
+         if(isAlive(x,y) && (checkCellCountAroundCell(x, y) > 3)) {
+           t.setDead(x,y);
+         }
+      }
+    }
+    printCell();
+    t.printCell();
+    
+    return (ILife) t; 
+    //ILife nextGen = t.nextGeneration();
+    //soll iLife zurückgeben
+    // return null;
   }
+  
+  public int checkCellCountAroundCell(int x, int y) {
+  //Der hardcoded scheiß funktioniert!
+  int cellCount = 0;
+
+    //center Cells
+    if((x > 0) && (x < 4) &&
+       (y > 0) && (y < 4)) {
+
+      if(isAlive(x-1, y-1))
+        cellCount++;
+      if(isAlive(x-1, y))
+        cellCount++;
+      if(isAlive(x-1, y+1))
+        cellCount++;
+      if(isAlive(x, y-1))
+        cellCount++;
+      if(isAlive(x, y+1))
+       cellCount++;
+      if(isAlive(x+1, y-1))
+       cellCount++;
+      if(isAlive(x+1, y))
+       cellCount++;
+      if(isAlive(x+1, y+1))
+       cellCount++;
+      //System.out.print("Center count at "+x+","+y+" = "+cellCount+"\n");
+    }
+
+    //corners
+    if(x == 0 && y == 0) {
+      if(isAlive(x+1, y))
+        cellCount++;
+      if(isAlive(x, y+1))
+        cellCount++;
+      if(isAlive(x+1, y+1))
+        cellCount++;
+      //System.out.print("Corner count at "+x+","+y+" = "+cellCount+"\n");
+    }
+
+    if(x == 0 && y == 4) {
+      if(isAlive(x, y-1))
+        cellCount++;
+      if(isAlive(x+1, y))
+        cellCount++;
+      if(isAlive(x+1, y-1))
+        cellCount++;
+      //System.out.print("Corner count at "+x+","+y+" = "+cellCount+"\n");
+    }
+
+    if(x == 4 && y == 0) {
+      if(isAlive(x, y+1))
+        cellCount++;
+      if(isAlive(x-1, y))
+        cellCount++;
+      if(isAlive(x-1, y+1))
+        cellCount++;
+      //System.out.print("Corner count at "+x+","+y+" = "+cellCount+"\n");
+    }
+
+    if(x == 4 && y == 4) {
+      if(isAlive(x, y-1))
+        cellCount++;
+      if(isAlive(x-1, y))
+        cellCount++;
+      if(isAlive(x-1, y-1))
+        cellCount++;
+      //System.out.print("Corner count at "+x+","+y+" = "+cellCount+"\n");
+    }
+
+    //sides
+    if(x == 0 && 4 > y && y > 0) {
+      if(isAlive(x+1, y-1))
+        cellCount++;
+      if(isAlive(x+1, y))
+        cellCount++;
+      if(isAlive(x+1, y+1))
+        cellCount++;
+      //System.out.print("Side   count at "+x+","+y+" = "+cellCount+"\n");
+    }
+
+    if(x == 4 && 4 > y && y > 0) {
+      if(isAlive(x-1, y-1))
+        cellCount++;
+      if(isAlive(x-1, y))
+        cellCount++;
+      if(isAlive(x-1, y+1))
+        cellCount++;
+      //System.out.print("Side   count at "+x+","+y+" = "+cellCount+"\n");
+    }
+
+    if(y == 0 && 4 > x && x > 0) {
+      if(isAlive(x-1, y+1))
+        cellCount++;
+      if(isAlive(x, y+1))
+        cellCount++;
+      if(isAlive(x+1, y+1))
+        cellCount++;
+      //System.out.print("Side   count at "+x+","+y+" = "+cellCount+"\n");
+    }
+
+    if(y == 4 && 4 > x && x > 0) {
+      if(isAlive(x-1, y-1))
+        cellCount++;
+      if(isAlive(x, y-1))
+        cellCount++;
+      if(isAlive(x+1, y-1))
+        cellCount++;
+      //System.out.print("Side   count at "+x+","+y+" = "+cellCount+"\n");
+    }
+    return cellCount;
+  }
+
+
+  // https://www.baeldung.com
+  public String replaceChar(String str, char ch, int position) {
+    String temp = str.substring(0, position) + ch + str.substring(position+1);
+    return temp;
+  }
+
+  public void printCell() {
+		// TODO Auto-generated method stub
+    for(int i = 0; i < cellMap.length; i++) {
+      System.out.print(cellMap[i]+ "\n");
+    }
+	}
+
+
+  public void printCell(String[] map) {
+		// TODO Auto-generated method stub
+    for(int i = 0; i < map.length; i++) {
+      System.out.print(map[i]+ "\n");
+    }
+	}
+
+
 }
--- LifeTest.java
+++ LifeTest.java
@@ -3,9 +3,13 @@ import static org.junit.Assert.*;
 
 public class LifeTest {
     
+    
+
     @Test
-    public void createNewCell() {
-        
+    public void createNewCell() {    
+
+        System.out.println("Test 1 - createNewCell()");
+
         // Arrange: drei lebende Zellen
         Life l = new Life();
         l.setAlive(0, 0);
@@ -22,17 +26,54 @@ public class LifeTest {
 
     @Test
     public void destroyLonelyCell() {
+       System.out.println("Test 2 - destroyLonelyCell()");
+        // Arrange: eine lebende Zelle
+        Life l = new Life();
+        l.setAlive(2, 2);
+        
+        // Act: Berechnung der Folgegeneration
+        ILife nextGen = l.nextGeneration();
+
+        // Assert: Rasterpunkt mit drei Nachbarn sollte jetzt leben
+        assertFalse(nextGen.isAlive(2, 2));
     }
+    
 
 
     @Test
-    public void keepAliveCell() {
+    public void keepAliveCell() { 
+      
+       System.out.println("Test 3 - keepAliveCell()");
+        // Arrange: zwei oder drei lebende Zellen
+        Life l = new Life();
+        l.setAlive(2, 2);
+        l.setAlive(2, 3);
+        l.setAlive(2, 1);
+        //l.setAlive(3, 3);
+
+        // Act: Berechnung der Folgegeneration
+        ILife nextGen = l.nextGeneration();
+
+        // Assert: Rasterpunkt mit drei Nachbarn sollte jetzt leben
+        assertTrue(nextGen.isAlive(2, 2));
     }
 
 
     @Test
     public void destroyCrowdedCell() {
+        System.out.println("Test 4 - destroyCrowdedCell()");
+     // Arrange: mehr als drei lebende Zellen
+        Life l = new Life();
+        l.setAlive(2, 2);
+        l.setAlive(2, 3);
+        l.setAlive(2, 1);
+        l.setAlive(1, 2);
+        l.setAlive(3, 2);
+
+        // Act: Berechnung der Folgegeneration
+        ILife nextGen = l.nextGeneration();
+
+        // Assert: Rasterpunkt mit drei Nachbarn sollte jetzt leben
+        assertFalse(nextGen.isAlive(2, 2));
     }
-
-
 }
